66acc12a9acbfe4d0d1b0fbf6bc6fec7e81b3c22
Bruno <bruno@Generals-MacBook-Air-8.local>
2018-04-11 10:00:52 -0400

    Add SignOut functionality

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,15 +2,33 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import * as routes from '../constants/routes';
+import SignOut from './SignOut';
+import SignIn from './SignIn';
 
-const Navigation = () =>
-<div>
+const Navigation = ({ authUser }) =>
+  <div>
+    { authUser ?
+      <NavigationAuth /> :
+      <NavigationNonAuth />
+    }
+  </div>
+
+const NavigationAuth = () =>
+  <div>
     <ul>
-      <li><Link to={routes.SIGN_IN}>Sign In</Link></li>
       <li><Link to={routes.LANDING}>Landing</Link></li>
       <li><Link to={routes.HOME}>Home</Link></li>
       <li><Link to={routes.ACCOUNT}>Account</Link></li>
+      <li><SignOut /></li>
     </ul>
-</div>
+  </div>
+
+const NavigationNonAuth = () =>
+  <div>
+    <ul>
+      <li><Link to={routes.LANDING}>Landing</Link></li>
+      <li><Link to={routes.SIGN_IN}>SignIn</Link></li>
+    </ul>
+  </div>
 
 export default Navigation;
